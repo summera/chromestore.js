f725daea567940811c957124898f814cc012648a
fendyg <fendygiovanni@gmail.com>
2013-11-15 15:36:51 -0800

    Add changeSourceToLocal function in demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,12 @@
 $(function(){
 	//FUNCTIONS
+	function changeSourceToLocal(fileEntry) {
+		var video = $("#vid").get(0);
+		var currentTime = video.currentTime;
+
+		$("video").attr("src", fileEntry.toURL());
+		video.currentTime = currentTime;
+	}
 
 	//EVENT HANDLERS
 	$("#fb").click(function(){
