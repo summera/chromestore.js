4314262d0dc5a00fa31f6ef0793b0b9af2cb9805
Ari Summer <aribsummer@gmail.com>
2013-11-19 20:24:40 -0800

    fixed demo to work with new methods using flags

--- demo.js
+++ demo.js
@@ -1,44 +1,44 @@
 $(function(){
-	//FUNCTIONS
-	function changeSourceToLocal(fileEntry) {
-		var video = $("#vid").get(0);
-		var isPlaying = !video.paused;
-		var currentTime = video.currentTime;
-		console.log(currentTime);
+    //FUNCTIONS
+    function changeSourceToLocal(fileEntry) {
+        var video = $("#vid").get(0);
+        var isPlaying = !video.paused;
+        var currentTime = video.currentTime;
+        console.log(currentTime);
 
 
-		$("video").on('loadedmetadata', function(){
-			console.log(currentTime);
-			video.currentTime = currentTime;
+        $("video").on('loadedmetadata', function(){
+            console.log(currentTime);
+            video.currentTime = currentTime;
 
-			if(isPlaying){
-				video.play();
-			}
-		});
+            if(isPlaying){
+                video.play();
+            }
+        });
 
-		$("video").attr("src", fileEntry.toURL());
-	}
+        $("video").attr("src", fileEntry.toURL());
+    }
 
-	var cs = new ChromeStore();
-	cs.init(1024*1024*1024);
+    var cs = new ChromeStore();
+    cs.init(1024*1024*1024);
 
 
-	//EVENT HANDLERS
-	$("#fb").click(function(){
-		var vidSource = $("#url").val();
-		$("#vid").attr("src",vidSource);
-		var video = $("#vid").get(0);
-		video.load();
-		//video.play();
-	});
+    //EVENT HANDLERS
+    $("#fb").click(function(){
+        var vidSource = $("#url").val();
+        $("#vid").attr("src",vidSource);
+        var video = $("#vid").get(0);
+        video.load();
+        //video.play();
+    });
 
-	$("#downloadToLocal").click(function(){
-		var url = $("#vid").attr('src');
-		cs.getAndWrite(url, 'video.mp4', 'video/mp4', true, function(fileEntry){
-			changeSourceToLocal(fileEntry);
-			console.log('Video source changed to local storage.');
-		});
+    $("#downloadToLocal").click(function(){
+        var url = $("#vid").attr('src');
+        cs.getAndWrite(url, 'video.mp4', 'video/mp4', {create: true}, function(fileEntry){
+            changeSourceToLocal(fileEntry);
+            console.log('Video source changed to local storage.');
+        });
 
-	});	
+    }); 
 
 });
--- test-chromestore.js
+++ test-chromestore.js
@@ -3,136 +3,136 @@ var cs;
 
 //Test writing to file
 function testwrite(cs){
-	//getDir(root, path, [callback])
-	cs.getDir('genres/action', {create: true}, function(){
-		//write(path, type, data, createFlag)
-		cs.write('genres/action/media.mp4','video/mp4','aaa', {create: true});
-	});
+    //getDir(root, path, [callback])
+    cs.getDir('genres/action', {create: true}, function(){
+        //write(path, type, data, createFlag)
+        cs.write('genres/action/media.mp4','video/mp4','aaa', {create: true});
+    });
 }
 
 //Test creating directory
 function testCreateDir(cs){
-	//create dir
-	cs.getDir('genres/drama', {create: true}, function(){
-		cs.getDir('genres/drama/extremedrama', {create: true}, function(){
-			cs.write('genres/drama/extremedrama/extreme.txt', 'text/plain', 'this is extreme!!', {create: true});
-			cs.getDir('genres/drama/blah', {create: true});
-		});
-	});
+    //create dir
+    cs.getDir('genres/drama', {create: true}, function(){
+        cs.getDir('genres/drama/extremedrama', {create: true}, function(){
+            cs.write('genres/drama/extremedrama/extreme.txt', 'text/plain', 'this is extreme!!', {create: true});
+            cs.getDir('genres/drama/blah', {create: true});
+        });
+    });
 
-	//create dir 
-	cs.getDir('genres/rock', {create: true}, function(){});
+    //create dir 
+    cs.getDir('genres/rock', {create: true}, function(){});
 }
 
 //Test deleting directory
 function testDeleteDir(cs){
-	cs.getDir('genres/deleteFolder', {create: true}, function(){
-		//deleteDir(path,[callback])
-		cs.deleteDir('genres/deleteFolder', {recursive: false});
-	});
+    cs.getDir('genres/deleteFolder', {create: true}, function(){
+        //deleteDir(path,[callback])
+        cs.deleteDir('genres/deleteFolder', {recursive: false});
+    });
 }
 
 //Test deleting non empty directory
 function testDeleteNonEmptyDir(cs){
-	cs.getDir('genres/notEmpty', {create: true}, function(){
-		cs.write('genres/notEmpty/media.mp4','video/mp4','aaa', {create: true}, function(){
-			cs.deleteDir('genres/notEmpty', {recursive: true});
-		});
-	});
+    cs.getDir('genres/notEmpty', {create: true}, function(){
+        cs.write('genres/notEmpty/media.mp4','video/mp4','aaa', {create: true}, function(){
+            cs.deleteDir('genres/notEmpty', {recursive: true});
+        });
+    });
 }
 
 //Test renaming directory
 function testRenameDir(cs){
-	cs.getDir('genres/rap', {create: true}, function(){
-		//renameDir(oldPath,newDirName,[callback])
-		cs.renameDir('genres/rap','renamedDir');
-	});
+    cs.getDir('genres/rap', {create: true}, function(){
+        //renameDir(oldPath,newDirName,[callback])
+        cs.renameDir('genres/rap','renamedDir');
+    });
 }
 
 //Test creating file
 function testCreateFile(cs){
-	cs.getFile('fileCreate.txt', {create: true, exclusive: true}, function(){
-		cs.write('fileCreate.txt', 'text/plain', 'test create file', {create: false});
-	});
+    cs.getFile('fileCreate.txt', {create: true, exclusive: true}, function(){
+        cs.write('fileCreate.txt', 'text/plain', 'test create file', {create: false});
+    });
 }
 
 //Test deleting file
 function testDeleteFile(cs){
-	cs.getFile('fileDelete.txt', {create: true, exclusive: true}, function(){
-		cs.write('fileDelete.txt', 'text/plain', 'test delete file', {create: false}, function(){
-			cs.deleteFile('fileDelete.txt');
-		});
-	});
+    cs.getFile('fileDelete.txt', {create: true, exclusive: true}, function(){
+        cs.write('fileDelete.txt', 'text/plain', 'test delete file', {create: false}, function(){
+            cs.deleteFile('fileDelete.txt');
+        });
+    });
 }
 
 //Test renaming file
 function testRenameFile(cs){
-	cs.getFile('fileNotRenamed.txt', {create: true, exclusive: true}, function(){
-		cs.write('fileNotRenamed.txt', 'text/plain', 'test rename file', {create: false}, function(){
-			cs.renameFile('fileNotRenamed.txt', 'fileRenamed.txt');
-		});
-	});
+    cs.getFile('fileNotRenamed.txt', {create: true, exclusive: true}, function(){
+        cs.write('fileNotRenamed.txt', 'text/plain', 'test rename file', {create: false}, function(){
+            cs.renameFile('fileNotRenamed.txt', 'fileRenamed.txt');
+        });
+    });
 }
 
 //Test getting data from url
 function testGetData(cs){
-	var url = 'https://s3.amazonaws.com/lr-chaos/videos/encoded_files/000/000/548/original/Hands-Elegant-Road-04-22-13.mp4';
-	console.log('Retrieving data from ' + url);
-	cs.getData(url, function(data){
-		
-		console.log('Bytes Received from ' + url + ': ' + data.byteLength);
-	});
+    var url = 'https://s3.amazonaws.com/lr-chaos/videos/encoded_files/000/000/548/original/Hands-Elegant-Road-04-22-13.mp4';
+    console.log('Retrieving data from ' + url);
+    cs.getData(url, function(data){
+        
+        console.log('Bytes Received from ' + url + ': ' + data.byteLength);
+    });
 }
 
 //Test getting and writing data from url to local file
 function testGetAndWrite(cs){
-	var url = 'https://s3.amazonaws.com/lr-chaos/videos/encoded_files/000/000/548/original/Hands-Elegant-Road-04-22-13.mp4';
-	console.log('Retrieving data from ' + url);
-	cs.getAndWrite(url, 'video.mp4', 'video/mp4', {create: true}, function(){
-		
-		console.log('Write video complete');
-	});
+    var url = 'https://s3.amazonaws.com/lr-chaos/videos/encoded_files/000/000/548/original/Hands-Elegant-Road-04-22-13.mp4';
+    console.log('Retrieving data from ' + url);
+    cs.getAndWrite(url, 'video.mp4', 'video/mp4', {create: true}, function(){
+        
+        console.log('Write video complete');
+    });
 }
 
 //Test used and remaining
 function testUsedRemaining(cs){
-	cs.usedAndRemaining(function (used,remaining) {
-		console.log("Used bytes: "+ used);
-		console.log("Remaining bytes: "+ remaining);
-	});
+    cs.usedAndRemaining(function (used,remaining) {
+        console.log("Used bytes: "+ used);
+        console.log("Remaining bytes: "+ remaining);
+    });
 }
 
 //Run tests
 function tests(cs){
-	testwrite(cs);
-	testCreateDir(cs);
-	testDeleteDir(cs);
-	testDeleteNonEmptyDir(cs);
-	testRenameDir(cs);
-	testCreateFile(cs);
-	testDeleteFile(cs);
-	testRenameFile(cs);
-	testUsedRemaining(cs);
-	//testGetData(cs);
-	//testGetAndWrite(cs);
+    testwrite(cs);
+    testCreateDir(cs);
+    testDeleteDir(cs);
+    testDeleteNonEmptyDir(cs);
+    testRenameDir(cs);
+    testCreateFile(cs);
+    testDeleteFile(cs);
+    testRenameFile(cs);
+    testUsedRemaining(cs);
+    //testGetData(cs);
+    //testGetAndWrite(cs);
 }
 
 
 window.onload = function(){
 
-	var cs = new ChromeStore();
-	cs.init(1024*1024*1024, tests);
-	$("#purgeButton").on("click",function(){
-		cs.purge();
-	});
+    var cs = new ChromeStore();
+    cs.init(1024*1024*1024, tests);
+    $("#purgeButton").on("click",function(){
+        cs.purge();
+    });
 
-	//Run get data test
-	$("#getDataButton").on("click",function(){
-		testGetData(cs);
-	});
+    //Run get data test
+    $("#getDataButton").on("click",function(){
+        testGetData(cs);
+    });
 
-	//Run get data and write test
-	$("#getAndWriteDataButton").on("click",function(){
-		testGetAndWrite(cs);
-	});
+    //Run get data and write test
+    $("#getAndWriteDataButton").on("click",function(){
+        testGetAndWrite(cs);
+    });
 }
